5504accd802d6c92f5d66768393453711da0cbf4
Ben Moorlach <moorl007@umn.edu>
2022-11-10 16:03:46 -0600

    add scripts

---
+++ neural-disaggregator/RNN/h5ToCsv.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# to run
+# python3 h5ToCsv.py ukdale.h5 > ukdale.csv
+import pandas as pd
+import sys
+print('hello world')
+fpath = sys.argv[1]
+print(fpath)
+
+print('printing hdf keys')
+
+
+outpath = 'CSVFiles/UKDALE'
+with pd.HDFStore(fpath, 'r') as hdf:
+    # This prints a list of all group names:
+    print(hdf.keys())
+    print(len(hdf.keys()))
+    count = 0
+    for key in hdf.keys():  
+        print(count) 
+        df = pd.read_hdf(fpath, key=key)
+        df.to_csv(outpath+key+'.csv')
+        count += 1
